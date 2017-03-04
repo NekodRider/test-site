ca3847c9414a8a496a6f64d84c956db9a13191c8
dRider2223 <Driver2223@gmail.com>
2017-03-04 23:18:33 +0800

    fix firefox(remove xhr-error)

--- app/static/main_v1.js
+++ app/static/main_v1.js
@@ -3,7 +3,6 @@ function uploadFile() {
     fd.append("fileToUpload", document.getElementById('fileToUpload').files[0]);
     var xhr = new XMLHttpRequest();
     xhr.upload.addEventListener("progress", uploadProgress, false);
-    xhr.addEventListener("error", uploadFailed, false);
     xhr.addEventListener("abort", uploadCanceled, false);
     xhr.open("POST","/");//修改成自己的接口
     xhr.send(fd);
@@ -17,10 +16,6 @@ function uploadProgress(evt) {
     else {
         document.getElementById('progressNumber').innerHTML = 'unable to compute';
     }
-}
-function uploadFailed(evt) {
-    alert("上传错误！请重新上传！");
-    window.location.replace(location.href);
 }
 function uploadCanceled(evt) {
     alert("上传中断！请重新上传！");
