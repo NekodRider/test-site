f0b89e69a44849a682621cdc35f30753b47d3dfe
dRider2223 <Driver2223@gmail.com>
2017-03-04 22:09:38 +0800

    add some effect on progress

--- app/static/main.js
+++ app/static/main.js
@@ -1,15 +1,3 @@
-function fileSelected() {
-    var file = document.getElementById('fileToUpload').files[0];
-    if (file) {
-        var fileSize = 0;
-        if (file.size > 1024 * 1024)
-            fileSize = (Math.round(file.size * 100 / (1024 * 1024)) / 100).toString() + 'MB';
-        else
-            fileSize = (Math.round(file.size * 100 / 1024) / 100).toString() + 'KB';
-        document.getElementById('fileName').innerHTML = 'Name: ' + file.name;
-        document.getElementById('fileSize').innerHTML = 'Size: ' + fileSize;
-    }
-}
 function uploadFile() {
     var fd = new FormData();
     fd.append("fileToUpload", document.getElementById('fileToUpload').files[0]);
@@ -17,13 +5,14 @@ function uploadFile() {
     xhr.upload.addEventListener("progress", uploadProgress, false);
     xhr.addEventListener("error", uploadFailed, false);
     xhr.addEventListener("abort", uploadCanceled, false);
-    xhr.open("POST","/");
+    xhr.open("POST","/");//修改成自己的接口
     xhr.send(fd);
 }
 function uploadProgress(evt) {
     if (evt.lengthComputable) {
         var percentComplete = Math.round(evt.loaded * 100 / evt.total);
         document.getElementById('progressNumber').innerHTML = percentComplete.toString() + '%';
+        document.getElementById('progress-bar').style.width=percentComplete.toString() + '%';
     }
     else {
         document.getElementById('progressNumber').innerHTML = 'unable to compute';
@@ -31,7 +20,9 @@ function uploadProgress(evt) {
 }
 function uploadFailed(evt) {
     alert("There was an error attempting to upload the file.");
+    window.location("");
 }
 function uploadCanceled(evt) {
     alert("The upload has been canceled by the user or the browser dropped the connection.");
+    window.location("");
 }
