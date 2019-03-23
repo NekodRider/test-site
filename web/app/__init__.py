a128b9eb611c8f336a9d3685d66d84caccd5eeb6
colinaaa <qingyu.wang@aliyun.com>
2019-03-24 00:52:22 +0800

    fix(web): change max content length to 256M

--- web/app/__init__.py
+++ web/app/__init__.py
@@ -17,6 +17,6 @@ app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 app.config['DOWNLOAD_FOLDER'] = DOWNLOAD_FOLDER
 app.config['STORAGE_FOLDER'] = STORAGE_FOLDER
 app.config['SECRET_KEY'] = 'unique-studio'
-app.config['MAX_CONTENT_LENGTH'] = 100 * 1024 * 1024
+app.config['MAX_CONTENT_LENGTH'] = 256 * 1024 * 1024
 
 from app import views
